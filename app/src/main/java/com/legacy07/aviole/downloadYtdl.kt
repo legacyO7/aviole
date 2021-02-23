1b91b00332a7c52b4ed60810bff9626ce3254f6b
Anit Sebastian <anitsebastian802@gmail.com>
2021-02-23 14:58:22 +0530

    download ytdl gui

--- app/src/main/java/com/legacy07/aviole/downloadYtdl.kt
+++ app/src/main/java/com/legacy07/aviole/downloadYtdl.kt
@@ -1,48 +1,125 @@
 package com.legacy07.aviole
 
+import android.app.ProgressDialog
 import android.content.Context
 import android.os.AsyncTask
 import android.os.Build
+import android.os.PowerManager
+import android.os.PowerManager.WakeLock
+import android.widget.Toast
 import androidx.annotation.RequiresApi
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
+import java.net.HttpURLConnection
 import java.net.URL
 
-class downloadYtdl(context: Context, path: String) : AsyncTask<Void, Void, Boolean>() {
-    val filePath = "$path/youtube-dl";
-    val folderPath = "$path";
-    val tarPath = "$path/avioleTPeM.tar.gz";
-    private val context = context;
-    val ytdl: File = File(filePath)
-    override fun doInBackground(vararg params: Void?): Boolean {
-        ytdl.setExecutable(true)
-        ytdl.canExecute()
 
-        if (ytdl.exists())
-            logger("file exists")
-        else {
-            HttpsTrustManager.allowAllSSL()
-            URL("https://yt-dl.org/downloads/latest/youtube-dl")
-                .openStream().use { input ->
-                    logger(input.toString())
-                    FileOutputStream(filePath).use { output ->
-                        input.copyTo(output)
-                    }
-                }
+class downloadYtdl(private val context: Context, path: String, progressDialog: ProgressDialog) : AsyncTask<String, Int, String>() {
+    val filePath = "$path/youtube-dl";
+    val folderPath = path;
+    val tarPath = "$path/avioleTPeM.tar.gz";
+    val ytdl: File = File(filePath)
+     var mWakeLock: WakeLock? = null
+     var mProgressDialog=progressDialog
+
+    override fun doInBackground(vararg params: String?): String {
+
+        if (ytdl.exists() && false) {
             ytdl.setExecutable(true)
+            logger("file exists")
+        } else {
+            /*  try {
+           //       HttpsTrustManager.allowAllSSL()
+   //            URL("https://yt-dl.org/downloads/latest/youtube-dl")
+   //                .openStream()
+               var u:URLConnection= URL("https://yt-dl.org/downloads/latest/youtube-dl").openConnection()
+                  u.connect()
+                 logger(u.contentLength.toString())
+
+                  URL("http://192.168.85.120/The_Greatest_Showman.mp3")
+                      .openStream().use { input ->
+                        logger(input.available().toString() + "-=-=-=-=-=")
+
+                          FileOutputStream(filePath).use { output ->
+                              input.copyTo(output)
+                          }
+                      }
+              }catch (e: IOException){
+                  logger(e.toString())
+              }
+               ytdl.setExecutable(true)
+           }
+   */
+            var input: InputStream? = null
+            var output: OutputStream? = null
+            var connection: HttpURLConnection? = null
+            try {
+                val url = URL(params[0])
+                HttpsTrustManager.allowAllSSL()
+                connection = url.openConnection() as HttpURLConnection
+                connection.connect()
+                // this will be useful so that you can show a typical 0-100% progress bar
+                val fileLength = connection.contentLength
+
+                // download the file
+                input = BufferedInputStream(url.openStream())
+                output = FileOutputStream(filePath)
+                val data = ByteArray(1024)
+                var total: Long = 0
+                var count: Int
+                while (input.read(data).also { count = it } != -1) {
+                    total += count.toLong()
+                    // publishing the progress....
+                    publishProgress((total * 100 / fileLength).toInt())
+                    output.write(data, 0, count)
+                }
+                output.flush()
+                output.close()
+                input.close()
+            } catch (e: Exception) {
+                return e.toString();
+            } finally {
+                try {
+                    output?.close();
+                    input?.close();
+                } catch (ignore: IOException) {
+                }
+
+                connection?.disconnect();
+            }
         }
-        return true;
+        return "null";
     }
 
     override fun onPreExecute() {
         super.onPreExecute()
-
+        val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        mWakeLock = pm.newWakeLock(
+            PowerManager.PARTIAL_WAKE_LOCK,
+            javaClass.name
+        )
+        mWakeLock?.acquire(10 * 60 * 1000L /*10 minutes*/)
+        mProgressDialog.show()
         logger("started")
     }
 
+    override fun onProgressUpdate(vararg progress: Int?) {
+        super.onProgressUpdate(*progress)
+        mProgressDialog.isIndeterminate = false
+        mProgressDialog.max = 100
+        mProgressDialog.progress = progress[0]!!
+    }
+
+
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onPostExecute(result: Boolean) {
+    override fun onPostExecute(result: String) {
         super.onPostExecute(result)
+        mWakeLock?.release();
+        mProgressDialog.dismiss();
+        if (result != "null")
+            Toast.makeText(context, "Download error: $result", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(context,"File downloaded", Toast.LENGTH_SHORT).show();
+
 
         logger("Done !!!")
         if (!File("$folderPath/files").exists()) {
@@ -56,11 +133,11 @@ class downloadYtdl(context: Context, path: String) : AsyncTask<Void, Void, Boole
         } else {
             //   changePermission(File("$folderPath/files/usr/bin/youtube-dl"))
 
-            executeAction(
-                folderPath,
-                "$folderPath/files/usr/bin/python",
-                arrayOf("youtube-dl", "-v")
-            )
+//            executeAction(
+//                folderPath,
+//                "$folderPath/files/usr/bin/python",
+//                arrayOf("youtube-dl", "-v")
+//            )
         }
 
     }
