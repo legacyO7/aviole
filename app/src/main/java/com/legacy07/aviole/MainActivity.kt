877c9d5715ff29556895a0d67a23137f9af93a62
Anit Sebastian <anitsebastian802@gmail.com>
2021-02-24 10:59:01 +0530

    download AvTPeM

--- app/src/main/java/com/legacy07/aviole/MainActivity.kt
+++ app/src/main/java/com/legacy07/aviole/MainActivity.kt
@@ -5,10 +5,13 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.legacy07.aviole.env.*
+import com.legacy07.aviole.misc.*
 import com.legacy07.aviole.ui.avHome
 import java.io.File
 
@@ -17,11 +20,13 @@ class MainActivity : AppCompatActivity() {
     var mProgressDialog: ProgressDialog? = null
     var alertDialogBuilder: AlertDialog.Builder? = null
 
+    var setupbutton: Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         appPath = packageManager.getPackageInfo(packageName, 0).applicationInfo.dataDir;
-        tarPath = "$appPath/miraklebox.tar.gz";
+        tarPath = "$appPath/avioleTPeM.tar.gz";
         ytdlPath = "$appPath/youtube-dl";
         homePath = "$appPath/files/home";
         prefixPath = "$appPath/files/usr";
@@ -48,6 +53,8 @@ class MainActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
         setContentView(R.layout.activity_main)
+        setupbutton=findViewById(R.id.setup_button)
+
 
     }
 
@@ -87,30 +94,22 @@ class MainActivity : AppCompatActivity() {
         val ytdl_bin: File = File(ytdlPath)
 
         if (!ytdl_bin.exists()) {
-            mProgressDialog?.setMessage("Downloading youtube-dl binary")
-            mProgressDialog?.setIndeterminate(true);
-            mProgressDialog?.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            mProgressDialog?.setCancelable(true);
-
-            val downloadFile = downloadYtdl(this, appPath, mProgressDialog!!)
-            downloadFile.execute("https://yt-dl.org/downloads/latest/youtube-dl")
-
-            mProgressDialog?.setOnCancelListener {
-                downloadFile.cancel(true) //cancel the task
-            }
+          initYTDLdownload(this,mProgressDialog!!)
         }
         if (!File(prefixPath).exists()) {
             if (File(tarPath).exists()) {
-                mProgressDialog?.setMessage("Extracting aviole module")
-                mProgressDialog?.setIndeterminate(true);
-                mProgressDialog?.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                extractTar(File(tarPath), File(appPath))
-                mProgressDialog?.cancel()
+              extract_aviole_tarball(mProgressDialog!!)
+            }else {
+              initAvioleModuleDownload(this,mProgressDialog!!)
             }
         }
         if(File(prefixPath).exists()&&ytdl_bin.exists()){
             startActivity(Intent(applicationContext,avHome::class.java))
             finish()
+        }else
+        {
+            setupbutton?.visibility= View.VISIBLE
+            setupbutton?.setOnClickListener { checkBlobs() }
         }
 
     }
